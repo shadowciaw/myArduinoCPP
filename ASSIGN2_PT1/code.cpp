52d03b30312e3af95cf7e5a29110a87fba990646
cmput274 <cmput274@ualberta.ca>
2018-11-02 14:29:27 -0600

    nov2_230pm

--- ASSIGN2_PT1/code.cpp
+++ ASSIGN2_PT1/code.cpp
@@ -119,9 +119,10 @@ uint16_t setup()
     return k;
 }
 
+/*
 uint8_t encryptIt((uint16_t) key, byte) {
     // encrypts a byte
-    
+
     uint8_t encryptedByte = byte ^ ((uint8_t) key);
 
     return encryptedByte;
@@ -134,6 +135,7 @@ void readSend(uint16_t k)
 
     while (Serial.available() == 0) {}
     uint8_t byte = Serial.read();
+    // ex. typed "C" will be an int represented by ascii
 
     // the first while loop waits for something to appear right?
     // if i wanna type something there should i keep it or no
@@ -170,13 +172,15 @@ void receive() {
     // decrypt it using shared secret key
     // send to serial monitor
 }
+*/
 
 int main()
 {
     uint16_t k = setup();
-
+    // shared secret key k returned from setup
+/*
     while (true) {
-        // use exlusive or operation for encryption/decryption
+        // use exlusive-or operation (^) for encryption/decryption
 
         // given a message character m, compute character e by exclusive or
         // with first 8 bits:
@@ -188,7 +192,7 @@ int main()
         readSend(k);
         receive();
     }
-
+*/
     Serial.flush();
 
     return 0;
--- ASSIGN2_PT1/code/code.cpp
+++ ASSIGN2_PT1/code/code.cpp
@@ -73,7 +73,7 @@ uint16_t generate_key(){
     uint16_t key = 0;
 
     for (int i = 0; i < 16; i++){
-        int bit = analogRead(idPin, BIN) & 1;
+        bit[i] = analogRead(idPin, BIN) & 1;
         // obtains least significant bit of analogRead
         key |= (bit << i);
         // shorthand for key = key | (bit << i );
@@ -119,11 +119,77 @@ uint16_t setup()
     return k;
 }
 
+uint8_t encryptIt((uint16_t) key, byte) {
+    // encrypts a byte
+
+    uint8_t encryptedByte = byte ^ ((uint8_t) key);
+
+    return encryptedByte;
+}
+
+void readSend(uint16_t k)
+{
+    // read a character (if available) from serial monitor
+    // send it to the other machine ENCRYPTED With help of shared secret key k
+
+    while (Serial.available() == 0) {}
+    uint8_t byte = Serial.read();
+    // ex. typed "C" will be 
+
+    // the first while loop waits for something to appear right?
+    // if i wanna type something there should i keep it or no
+    // if i typed a char would it be ok keeping it as uint16_t type...
+
+    //Serial.write(byteread);
+    // Serial.write writes binary byte to serial mon, which is interpreted as char
+
+    // Serial3.write() // writes BINARY data to serial port
+    // inside can be (val), (str), (buf, len)
+    // value to send as single byte
+    // str to send as series of bytes
+    // array to send as series of bytes and length of buffer
+
+    // do i really need this? its gonna print after every char right? how would i go about waiting for the entire thing then
+    // if i was to do what was done in readString() id have to know the length of the message first dont i
+    Serial.print(byte);
+
+    // why do they use uint32 and uint8 ?? why do we need to type cast it down to 8??? its 16 bits long how tf???
+    Serial3.write(encryptIt(k, byte));
+
+    if (byte == "\r") {
+        // send \r (carriage return)
+        Serial.print(encryptIt(k,"\r"));
+        
+        // follow with line feed char \n
+        Serial.print(encryptIt(k, "\n"))
+        // both are encrypted
+    }
+}
+
+void receive() {
+    // receive an encrypted byte (if available) from other machine
+    // decrypt it using shared secret key
+    // send to serial monitor
+}
 
 int main()
 {
     uint16_t k = setup();
     // shared secret key k returned from setup
+
+    while (true) {
+        // use exlusive-or operation (^) for encryption/decryption
+
+        // given a message character m, compute character e by exclusive or
+        // with first 8 bits:
+        // klow = k mod 256 
+        // of key k: e = m (exclusive or) klow
+
+        // since exclusive or is its own inverse, original message character m
+        // can be obtained from encrypted e with klow: m = e (exclusive or) klow
+        readSend(k);
+        receive();
+    }
 
     Serial.flush();
 
